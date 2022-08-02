5e04a871070634edd1d7d7dd17452edd7fb16a8e
Jan Stenkamp <46532815+jsten07@users.noreply.github.com>
2022-08-02 17:37:29 +0200

    Update get_pi_requirements.sh

--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
 
-# Get packages required for OpenCV
-
-sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
-sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get -y install libxvidcore-dev libx264-dev
-sudo apt-get -y install qt4-dev-tools libatlas-base-dev
-
-# Need to get an older version of OpenCV because version 4 has errors
-pip3 install opencv-python==3.4.6.27
-
-# Get packages required for TensorFlow
-# Using the tflite_runtime packages available at https://www.tensorflow.org/lite/guide/python
-# Will change to just 'pip3 install tensorflow' once newer versions of TF are added to piwheels
-
-#pip3 install tensorflow
-
-version=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
-
-if [ $version == "3.7" ]; then
-pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp37-cp37m-linux_armv7l.whl
-fi
-
-if [ $version == "3.5" ]; then
-pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp35-cp35m-linux_armv7l.whl
-fi
-
+# General
 pip3 install pyyaml
+pip3 install schedule
+pip3 install --upgrade numpy
 
-pip3 install python-crontab
+# Sensors
+sudo pip3 install adafruit-circuitpython-dht
+sudo apt-get install libgpiod2
 
-pip3 install requests
+# Microphone
+sudo pip3 install --upgrade adafruit-python-shell
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2smic.py
+y n | sudo python3 i2smic.py
+sudo apt-get install libatlas-base-dev libportaudio2 libasound-dev
+yes | python3 -m pip install --user sounddevice
+python3 -m pip install --user scipy
