51c98d39a354707fac665ce7c4603ae3193790df
Jan Stenkamp <46532815+jsten07@users.noreply.github.com>
2022-08-15 11:16:35 +0200

    Update get_pi_requirements.sh

--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -12,14 +12,17 @@ sudo apt-get install libgpiod2
 # Microphone
 sudo pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2smic.py
-y n | sudo python3 i2smic.py
-sudo apt-get install libatlas-base-dev libportaudio2 libasound-dev
+printf '%s\n' y n | sudo python3 i2smic.py
+yes | sudo apt-get install libatlas-base-dev libportaudio2 libasound-dev
 yes | python3 -m pip install --user sounddevice
 python3 -m pip install --user scipy
+pip3 install SoundFile
 
+# autostart
 yes | sudo apt-get install xterm
 mkdir -p ~/.config/autostart
 cp /home/pi/station/lxterm-autostart.desktop ~/.config/autostart
 
+# config interfaces -> enable camera and i2c
 sudo raspi-config nonint do_camera 0
 sudo raspi-config nonint do_i2c 0
